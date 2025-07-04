e42add437e94cf4372876a8db5802cb213cba02f
Zerogallo <167368572+Zerogallo@users.noreply.github.com>
2025-07-04 08:29:34 -0300

    up

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import logo from "../public/Devlogo.jpg";
+
+function App() {
+  return (
+    <>
+      <div className="contents">
+        <div className="logo">
+          <img src={logo} alt="logo" />
+          <h1>IAcss</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
