0930683e97fd0436a6ad4fab7b935f0dc073a08e
Zerogallo <167368572+Zerogallo@users.noreply.github.com>
2025-07-04 10:02:57 -0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import "./App.css";
 import logo from "../public/Devlogo.jpg";
+import { FaLaptopCode } from "react-icons/fa";
+import { BsChatDots } from "react-icons/bs";
+import { HiMagnifyingGlass } from "react-icons/hi2";
 
 function App() {
   return (
@@ -8,6 +11,24 @@ function App() {
         <div className="logo">
           <img src={logo} alt="logo" />
           <h1>IAcss</h1>
+        </div>
+        <div className="caixaTotal">
+          <div className="caixaCss">
+            <div className="caixaCodigo">
+              <FaLaptopCode />
+              <h1>Código CSS</h1>
+            </div>
+            <div className="caixaResultado">
+              <BsChatDots />
+              <h1>Resultado</h1>
+            </div>
+          </div>
+          <div className="caixaPesquisar">
+            <input type="text" placeholder="Digite sua pesquisar" />
+            <button>
+              <HiMagnifyingGlass /> Criar CSS
+            </button>
+          </div>
         </div>
       </div>
     </>
